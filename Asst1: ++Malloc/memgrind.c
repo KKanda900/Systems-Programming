fbfaf32e2f6b4d08182bc7828d3fae96805be771
Jim Choi <cjxian0304@gmail.com>
2020-10-25 18:03:13 -0400

    implement the last workload

--- Asst1: ++Malloc/memgrind.c
+++ Asst1: ++Malloc/memgrind.c
@@ -4,7 +4,7 @@
 #include <sys/time.h>
 #include "mymalloc.h"
 
-#define REPEAT 100000
+#define REPEAT 50
 
 struct timeval testA()
 {
@@ -128,12 +128,44 @@ struct timeval testD()
 struct timeval testE()
 {
     clean();
+    void* p1[60];
+    void* p2[60];
+    int malloc_count1 = 0, free_count1 = 0;
+    int doMalloc[120];
+    srand((unsigned)time(0));
+    for (int i = 0; i < 120; i++) doMalloc[i] = rand() % 2;
+
     struct timeval start_t;
     struct timeval end_t;
     struct timeval res_t;
     gettimeofday(&start_t, NULL);
 
-    // Implement test D here
+    for (int i = 0; i < 120; i++)
+    {
+        if (malloc_count1 <= free_count1 || (doMalloc[i] && malloc_count1 < 60))
+        {
+            if (DEBUG) printf("%d", malloc_count1);
+            p1[malloc_count1] = malloc(120 - malloc_count1*2);
+            malloc_count1++;
+        }
+        else
+        {
+            if (DEBUG) printf("%d", free_count1);
+            free(p1[free_count1]);
+            free_count1++;
+        }
+    }
+
+    for (int i = 0; i < 60; i++)
+    {
+        if (DEBUG) printf("%d", i);
+        p2[i] = malloc(45);
+    }
+    for (int i = 0; i < 60; i++)
+    {
+        if (DEBUG) printf("%d", i);
+        free(p2[i]);
+    }
 
     gettimeofday(&end_t, NULL);
     timersub(&end_t, &start_t, &res_t);
@@ -161,13 +193,13 @@ int main() {
         resultB[i] = testB();
         resultC[i] = testC();
         resultD[i] = testD();
-//        resultE[i] = testE();
+        resultE[i] = testE();
     }
     printf("Mean time for workload A: %.3fms\n", avgTime(resultA));
     printf("Mean time for workload B: %.3fms\n", avgTime(resultB));
     printf("Mean time for workload C: %.3fms\n", avgTime(resultC));
     printf("Mean time for workload D: %.3fms\n", avgTime(resultD));
-//    printf("Mean time for workload E: %.3fms\n", avgTime(resultE));
+    printf("Mean time for workload E: %.3fms\n", avgTime(resultE));
     return 0;
 
 /*    void* m[30];
--- Asst1: ++Malloc/mymalloc.h
+++ Asst1: ++Malloc/mymalloc.h
@@ -9,7 +9,7 @@ void *mymalloc(size_t size, const char *file, int line);
 void myfree(void *p, const char *file, int line);
 void clean();
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define malloc(size) mymalloc(size, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__) 
