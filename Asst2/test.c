a6da92807a0ab9ce8be2aa3a51a3e3a9150c41f8
Karun Kanda <kk951@java.cs.rutgers.edu>
2020-11-25 23:19:09 -0500

    some subdirectories and all the directories open

--- Asst2/test.c
+++ Asst2/test.c
@@ -8,6 +8,57 @@
  * Currently: Working on file handling
  */
 
+typedef struct thread_list
+{
+    unsigned long int data;
+    struct thread_list *next;
+} thread_list;
+
+thread_list *addNode(thread_list *front, unsigned long data)
+{
+    thread_list *newNode = malloc(sizeof(thread_list)); //malloc space for the newNode that you are creating
+    newNode->data = data;
+    newNode->next = NULL;
+
+    // if front is null, which is most likely when your first adding to front then
+    // make newNode the front
+    if (front == NULL)
+    {
+        front = newNode;
+        //free(newNode);
+        return front;
+    }
+
+    // Otherwise front is not null and you can keep adding to the front node
+    (*newNode).next = front->next;
+    front->next = newNode;
+
+    // In the end return front
+    return front;
+}
+
+void free_list(thread_list *front)
+{
+    // Like for other linked list functions, check if the front is null or not
+    if (front == NULL)
+    {
+        return;
+    }
+
+    //Set a pointer so we can iterate nicely
+    thread_list *ptr = front;
+    while (ptr->next != NULL)
+    {
+        // This would be like any traversal but we are freeing each memory of the pointers
+        thread_list *tmp = ptr->next;
+        free(ptr); // like here
+        ptr = tmp;
+    }
+
+    // then we free the rest
+    free(ptr);
+}
+
 void *directory_handling(void *d_path)
 {
     // do something
@@ -15,6 +66,9 @@ void *directory_handling(void *d_path)
     DIR *dir = opendir(d_path);
     struct dirent *dp;
     char localname[200000];
+    thread_list *front = malloc(sizeof(thread_list));
+    thread_list *front2 = malloc(sizeof(thread_list));
+    void *handler;
 
     if (dir != NULL)
     {
@@ -22,32 +76,50 @@ void *directory_handling(void *d_path)
         {
             if ((strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0))
                 continue;
-            strcpy(localname, d_path);
-            strcat(localname, "/");
-            strcat(localname, dp->d_name);
             if (dp->d_type == DT_DIR)
             {
-                printf("Directory Path: %s\n", localname);
+                strcpy(localname, d_path);
+                strcat(localname, "/");
+                strcat(localname, dp->d_name);
+                printf("%s\n", localname);
+                pthread_t ptid;
+                addNode(front, ptid);
+                pthread_create(&ptid, NULL, &directory_handling, (void *)localname);
             }
-            else if (dp->d_type == DT_REG) 
+            else if (dp->d_type == DT_REG)
             {
-                printf("File Path: %s\n", localname); 
-            }
-            else
-            {
-                continue;
+                strcpy(localname, d_path);
+                strcat(localname, "/");
+                strcat(localname, dp->d_name);
+                printf("%s\n", localname);
+                pthread_t ptid;
+                addNode(front2, ptid);
+                pthread_create(&ptid, NULL, &file_handling, (void *)localname);
             }
         }
     }
-    else
+    else if (dir == NULL)
     {
-        printf("%s\n", (char *)d_path);
         perror(d_path);
         exit(1);
     }
 
-    printf("Ending directory handler\n");
     closedir(dir);
+
+    while (front != NULL)
+    {
+        pthread_join(front->data, &handler);
+        front = front->next;
+    }
+
+    while (front2 != NULL)
+    {
+        pthread_join(front2->data, &handler);
+        front2 = front2->next;
+    }
+
+    free_list(front);
+    free_list(front2);
 
     return NULL;
 }
