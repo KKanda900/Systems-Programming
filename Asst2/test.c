6896ae1d9bd0c8653945121500053ce15d524ace
Karun Kanda <36608879+KKanda900@users.noreply.github.com>
2020-11-27 02:06:39 -0500

    segfaults make me puke

--- Asst2/test.c
+++ Asst2/test.c
@@ -16,15 +16,39 @@ typedef struct thread_list
     struct thread_list *next;
 } thread_list;
 
-typedef struct thread_arg {
-    char* pathname; 
-    pthread_mutex_t * mutex_ptr;
-} thread_arg;
+typedef struct path_names
+{
+    char data[200000];
+    struct path_names *next;
+} path_names;
 
 thread_list *addNode(thread_list *front, unsigned long data)
 {
     thread_list *newNode = malloc(sizeof(thread_list)); //malloc space for the newNode that you are creating
     newNode->data = data;
+    newNode->next = NULL;
+
+    // if front is null, which is most likely when your first adding to front then
+    // make newNode the front
+    if (front == NULL)
+    {
+        front = newNode;
+        //free(newNode);
+        return front;
+    }
+
+    // Otherwise front is not null and you can keep adding to the front node
+    (*newNode).next = front->next;
+    front->next = newNode;
+
+    // In the end return front
+    return front;
+}
+
+path_names *addNode2(path_names *front, char *data)
+{
+    path_names *newNode = malloc(sizeof(path_names)); //malloc space for the newNode that you are creating
+    strcpy(front->data, data);
     newNode->next = NULL;
 
     // if front is null, which is most likely when your first adding to front then
@@ -68,15 +92,38 @@ void free_list(thread_list *front)
     free(ptr);
 }
 
+void free_list2(path_names *front)
+{
+    // Like for other linked list functions, check if the front is null or not
+    if (front == NULL)
+    {
+        return;
+    }
+
+    //Set a pointer so we can iterate nicely
+    path_names *ptr = front;
+    while (ptr->next != NULL)
+    {
+        // This would be like any traversal but we are freeing each memory of the pointers
+        path_names *tmp = ptr->next;
+        free(ptr); // like here
+        //pthread_exit(NULL);
+        ptr = tmp;
+    }
+
+    // then we free the rest
+    free(ptr);
+}
+
 void *file_handling(void *p)
 {
     /* thread_arg * args = (thread_arg *) p;
-
     pthread_mutex_lock(args->mutex_ptr); */
 
-    int fd = open((char*)p, O_RDONLY);
-    printf("File: %s\n", (char*)p);
-    
+    /* int fd = open((char *)p, O_RDONLY); */
+    path_names*file = (path_names*)p;
+    printf("File: %s\n", file->data);
+
     /* pthread_mutex_unlock(args->mutex_ptr); */
     return NULL;
 }
@@ -85,10 +132,15 @@ void *directory_handling(void *d_path)
 {
     // do something
 
-    DIR *dir = opendir(d_path);
+    path_names *f = (path_names *)d_path;
+
+    DIR *dir = opendir(f->data);
     struct dirent *dp;
     char localname[200000];
     thread_list *front = malloc(sizeof(thread_list));
+
+    path_names *files = malloc(sizeof(path_names));
+
     //pthread_mutex_t lock;
 
     if (dir != NULL)
@@ -97,22 +149,25 @@ void *directory_handling(void *d_path)
         {
             if ((strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0))
                 continue;
-            strcpy(localname, d_path);
-            strcat(localname, "/");
-            strcat(localname, dp->d_name); 
             if (dp->d_type == DT_DIR)
             {
+                strcpy(localname, f->data);
+                strcat(localname, "/");
+                strcat(localname, dp->d_name);
                 printf("Directory Path: %s\n", localname);
-                
+
                 /* directory_handling((void*)localname); */
-                
+
                 pthread_t ptid;
                 addNode(front, ptid);
-                pthread_create(&ptid, NULL, &directory_handling, (void*)localname);
-                
+                addNode2(f, localname);
+                pthread_create(&ptid, NULL, &directory_handling, (void *)f);
             }
             else if (dp->d_type == DT_REG)
             {
+                strcpy(localname, f->data);
+                strcat(localname, "/");
+                strcat(localname, dp->d_name);
 
                 /* file_handling((void*)localname); */
 
@@ -122,25 +177,28 @@ void *directory_handling(void *d_path)
 
                 pthread_t ptid;
                 addNode(front, ptid);
-                pthread_create(&ptid, NULL, &file_handling, (void*)localname);
-                
+                addNode2(files, localname);
+                pthread_create(&ptid, NULL, &file_handling, (void *)files);
             }
         }
     }
-    else 
+    else
     {
-        perror(d_path);
+        perror(f->data);
         /* printf("Error\n"); */
         exit(1);
     }
 
     closedir(dir);
 
+    free_list2(f);
+    free_list2(files);
+
     free_list(front);
 
+    
     return NULL;
 }
-
 
 int main(int argc, char **argv)
 {
@@ -150,7 +208,10 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    directory_handling((void *)argv[1]);
+    path_names *f = malloc(sizeof(path_names));
+    addNode2(f, argv[1]);
+
+    directory_handling((void *)f);
 
     /* pthread_exit(NULL); */
 
